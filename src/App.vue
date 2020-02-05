d1ad1d117a2b8100e305f2b89d063fa4d079b7cf
Annina Ecker <anninaecker@MacBook-Pro-8.fritz.box>
2020-02-05 10:28:07 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,15 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-    <Header></Header>
-    </div>
-      <div class="col-md-12 mt-5">
+        <Header></Header>
+      </div>
+      <div class="col-md-12 mt-4">
         <transition name="slide" mode="out-in">
-        <router-view></router-view>
+          <router-view></router-view>
         </transition>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -19,17 +19,24 @@ import Header from "./components/Header";
 export default {
   components: {
     Header
+  },
+  created() {
+    this.$store.dispatch('initStocks');
   }
 };
 </script>
 
 <style lang="scss">
+body {
+  background-color: #0d2326;
+}
+
 .slide-enter-active {
   animation: slide-in 0.7s ease-out;
 }
 
 .slide-leave-active {
-  animation: slide-out 0.7s ease-out;
+  animation: slide-out 0.7s ease-out forwards;
 }
 
 @keyframes slide-in {
--- src/components/Stocks/Stock.vue
+++ src/components/Stocks/Stock.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="col-sm-6 col-lg-5 mb-4">
-    <div class="card border border-success">
-      <div class="card-header bg-success">
-        {{ stock.name }}
-        <small>(Price: {{ stock.price }} )</small>
-      </div>
-      <div class="card-body">
-        <input type="number" class="form-control" placeholder="Quantity" v-model="quantity" />
-        <br />
-        <button class="btn btn-success button">Buy</button>
+<div class="row">
+    <div class="col-12 mb-4 justify-content-center d-flex">
+        <div class="card border border-success">
+          <div class="card-header bg-success">
+            {{ stock.name }}
+            <small>(Price: {{ stock.price }} )</small>
+          </div>
+          <div class="card-body">
+            <input type="number" class="form-control" placeholder="Quantity" v-model.number="quantity"/>
+            <br />
+            <button class="btn btn-success button" @click="buyStock"
+            :disabled="isDisabled">Buy</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -22,9 +25,28 @@ export default {
     return {
       quantity: 0
     }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quanitity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  },
+  computed: {
+    isDisabled() {
+      return (this.quantity <= 0 || !Number.isInteger(this.quantity)) 
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.card {
+    width: 600px;
+}
 </style>
